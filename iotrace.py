e513fcf2ae0cdaa1141c37d2d83ac20e2e2d7af0
NiuGenen <602131568@qq.com>
2017-05-17 22:37:15 +0800

    processing more anlysis & drawing it

--- iotrace.py
+++ iotrace.py
@@ -3,38 +3,9 @@ import os
 import time
 
 #io traces from http://iotta.snia.org/
-test = False
 
-cwd = os.getcwd()
-print("CWD     = " + cwd)
 default_source_dir = "E:\DevelopmentToolsRelease"
 default_traces_dir = default_source_dir + "\Traces"
-os.chdir( default_source_dir )
-cwd = os.getcwd()
-print("CWD     = " + cwd)
-
-#define log_file
-default_log_file = default_source_dir + "\log.txt"
-log_file = open(default_log_file,"wb")
-log_count = 100000
-def log_line(info):
-    global log_count
-    log_count -= 1
-    time_now = int(time.time())
-    time_local = time.localtime(time_now)
-    dt = time.strftime("%Y-%m-%d %H:%M:%S", time_local)
-    log_file.write( ("[" + dt + "]    " + info + "\r\n").encode(encoding="utf-8") )
-    if log_count < 0:
-        log_file.flush()
-        log_count = 1000000
-def log_close():
-    log_file.close()
-#test log
-if test:
-    i = 1
-    while(i < 10):
-        log_line("test log " + str(i) )
-        i += 1
 
 #get trace file name
 files = os.listdir( default_traces_dir )
@@ -52,25 +23,17 @@ else:
 
 #file name format : tracename.dd-mm-yyyy.hh-mm-(PM|AM).trace
 def get_base_time_stramp_in_s_from_file_name(name):
-    #print("DECODE TIME from " + name)
     parts = name.split('.')
-    #for part in parts:
-    #    print(part)
     base_data_str = parts.__getitem__(1)
-    #print("DATA = " + base_data_str)
     base_time_str = parts.__getitem__(2)
-    #print("TIME = " + base_time_str)
     base_time_parts = base_time_str.split('-')
     base_time_h = int(base_time_parts.__getitem__(0))
     if base_time_parts.__getitem__(2) == "PM":
         base_time_h += 12
     base_time_m = int(base_time_parts.__getitem__(1))
     base_data_time_str = base_data_str + " " + str(base_time_h) + ":" + str(base_time_m)
-    #print("STR  = " + base_data_time_str )
     base_data_time = time.strptime(base_data_time_str, "%m-%d-%Y %H:%S")
-    #print( "DATA_TIME = " + base_data_time.__str__() )
     base_time_stramp = time.mktime( base_data_time )
-    #print("TIMESTRAMP in s  = " + str(base_time_stramp) )
     return int(base_time_stramp)
 
 #write dict to file
@@ -80,180 +43,168 @@ def write_dict_to_csv(filename, dict):
         file.write((str(d) + "," + str(dict[d]) + "\r\n").encode(encoding="utf-8"))
     file.close()
 
-if test:
-    '''
-    test one file
-    '''
-    f = files.__getitem__(0)
+min_base_time = get_base_time_stramp_in_s_from_file_name(files.__getitem__(0))
+for f in files:
     print(f)
-    print( "TEST FILE : " + f )
-    trace = open( default_traces_dir + r"\\" + f, "r")
-    #count = 1
-    processing = False
-    base_timestramp_in_s  = get_base_time_stramp_in_s_from_file_name( f )
-    print("TimeStramp in Seconds = " + str(base_timestramp_in_s) )
+    base_time = get_base_time_stramp_in_s_from_file_name(f)
+    print("Time = " + str(base_time) )
+    if(base_time < min_base_time):
+        min_base_time = base_time
+print("Min Time = " + str(min_base_time) )
+
+min_timestramp_s = 0
+max_timestramp_s = 0
+total_read = 0
+total_write = 0
+size_read = 0
+size_write = 0
+#count_s_2_read = dict()
+#count_s_2_write = dict()
+#count_byte_2_io = dict()
+map_disknum_2_s_2_io = dict()# each disknum to one dict, which is timestramp in second to io
+#list_ns_of_io = []
+map_ns_to_offset_in_sector = dict()
+def update_map_disknum_2_s_2_io(target, disknum,timestramp_s):
+    if disknum in target:
+        s_2_io = target[disknum]
+        if timestramp_s in s_2_io:
+            s_2_io[timestramp_s] += 1
+        else:
+            s_2_io.setdefault(timestramp_s, 1)
+    else:
+        target.setdefault(disknum, {timestramp_s:1})
+
+for f in files:
+    print("Processing : " + f)
+    file = open(default_traces_dir + r"\\" + f, "r")
+    base_timestramp_in_s = get_base_time_stramp_in_s_from_file_name(f)
     base_timestramp_in_ns = base_timestramp_in_s * 1000 * 1000
-    print("TimeStramp in Seconds = " + str(base_timestramp_in_ns) )
-    total_read = 0
-    total_write = 0
-    count_s_2_read = dict()
-    count_s_2_write = dict()
-    for line in trace:
+    print("Base time :" + str(base_timestramp_in_ns) )
+    processing = False
+    for line in file:
         line = line.strip().lower()
-        #if line == "beginheader":
-        #    count = -1000
-        #if count < 10 and count > 0:
         if processing:
-            #print(line)
-            #print("-----------------------------------------------------------")
             words = line.split(",")
-            #for word in words:
-            #    print(word.strip())
-            op_type = words.__getitem__(0)#diskread/diskwrite
-            try:
-                op_timestramp_rel_ns = words.__getitem__(1)#ns
-            except IndexError as e:
-                print("IndexError on line : " + line)
+            op_type = words.__getitem__(0)  # diskread/diskwrite
+            if op_type != "diskread" and op_type != "diskwrite":
                 continue
-            op_size = words.__getitem__(6)#byte
-            #print(op_type + " " + op_timestramp_rel_ns + " " + op_size)
-            op_timestramp_ab_ns = int(op_timestramp_rel_ns) + base_timestramp_in_ns
-            op_timestramp_ab_s = int(op_timestramp_ab_ns/1000/1000)
+
+            op_timestramp_rel_ns = words.__getitem__(1)  # relatived timestramp in ns
+            op_timestramp_ab_ns = int(op_timestramp_rel_ns) + base_timestramp_in_ns # absolute timestramp in ns
+            #list_ns_of_io.append(op_timestramp_ab_ns)   # update timestramp ns list # assume no requests has same timestramp in ns
+            op_timestramp_ab_s = int(op_timestramp_ab_ns / 1000 / 1000) # absolute timestramp in s
+            if min_timestramp_s == 0:
+                min_timestramp_s = op_timestramp_ab_s
+            else:
+                if op_timestramp_ab_s < min_timestramp_s:
+                    min_timestramp_s = op_timestramp_ab_s
+            if max_timestramp_s == 0:
+                max_timestramp_s = op_timestramp_ab_s
+            else:
+                if op_timestramp_ab_s > max_timestramp_s:
+                    max_timestramp_s = op_timestramp_ab_s
+
+            op_size = words.__getitem__(6)  # io size in byte 16
+            op_size = int(op_size, 16)
+            #if op_size in count_byte_2_io:
+            #    count_byte_2_io[op_size] += 1
+            #else:
+            #    count_byte_2_io.setdefault(op_size, 1)
+
+            op_disk_num = words.__getitem__(8) # disk number
+            op_disk_num = int(op_disk_num)
+            update_map_disknum_2_s_2_io(map_disknum_2_s_2_io, op_disk_num, op_timestramp_ab_s) # update disknum
+
+            op_offset_byte = words.__getitem__(5) # data offset in byte 16
+            op_offset_byte = int(op_offset_byte, 16)
+            op_offset_sector = int(op_offset_byte / 512) # data offset in sector
+            #map_ns_to_offset_in_sector.update( { op_timestramp_ab_ns: op_offset_sector } ) # timestramp of request to its data offset in sector
+
             if op_type == "diskread":
                 total_read += 1
-                if op_timestramp_ab_s in count_s_2_read:
-                    count_s_2_read[op_timestramp_ab_s] += 1
-                else:
-                    count_s_2_read.setdefault(op_timestramp_ab_s,1)
+                size_read += op_size
+                #if op_timestramp_ab_s in count_s_2_read:
+                #    count_s_2_read[op_timestramp_ab_s] += 1
+                #else:
+                #    count_s_2_read.setdefault(op_timestramp_ab_s, 1)
             else:
                 if op_type == "diskwrite":
                     total_write += 1
-                if op_timestramp_ab_s in count_s_2_write:
-                    count_s_2_write[op_timestramp_ab_s] += 1
-                else:
-                    count_s_2_write.setdefault(op_timestramp_ab_s,1)
-            #print(op_type + " " + str(op_timestramp) + " " + op_size)
-            #op_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(op_timestramp/1000000))
-            #print(op_type + " " + str(op_time) + " " + op_size)
-        #if count >= 10:
-        #    break
-        #count += 1
-        if line == "endheader":
-            #count = 0
-            processing = True
-
-    print("TotalRead  = " + str(total_read) )
-    print("TotalWrite = " + str(total_write) )
-    #print(count_s_2_read)
-    print(count_s_2_read.__len__())#2577
-    print(count_s_2_write.__len__())#3208
-else:
-    #not test
-    min_base_time = get_base_time_stramp_in_s_from_file_name(files.__getitem__(0))
-    for f in files:
-        #print(f)
-        base_time = get_base_time_stramp_in_s_from_file_name(f)
-        print("Time = " + str(base_time) )
-        #log_line("Time = " + str(base_time))
-        if(base_time < min_base_time):
-            min_base_time = base_time
-    print("Min Time = " + str(min_base_time) )
-    #min_timestramp_s = 0
-    #max_timestramp_s = 0
-    #total_read = 0
-    #total_write = 0
-    #size_read = 0
-    #size_write = 0
-    #count_s_2_read = dict()
-    #count_s_2_write = dict()
-    count_byte_2_io = dict()
-    for f in files:
-        print("Processing : " + f)
-        file = open(default_traces_dir + r"\\" + f, "r")
-        base_timestramp_in_s = get_base_time_stramp_in_s_from_file_name(f)
-        base_timestramp_in_ns = base_timestramp_in_s * 1000 * 1000
-        print("Base time :" + str(base_timestramp_in_ns) )
-        processing = False
-        for line in file:
-            line = line.strip().lower()
-            if processing:
-                words = line.split(",")
-                op_type = words.__getitem__(0)  # diskread/diskwrite
-                try:
-                    op_timestramp_rel_ns = words.__getitem__(1)  # ns
-                except IndexError as e:
-                    print("IndexError on line : " + line)
-                    continue
-                op_size = words.__getitem__(6)  # byte
-                op_size = int(op_size, 16)
-                # print(op_type + " " + op_timestramp_rel_ns + " " + op_size)
-                #op_timestramp_ab_ns = int(op_timestramp_rel_ns) + base_timestramp_in_ns
-                #op_timestramp_ab_s = int(op_timestramp_ab_ns / 1000 / 1000)
-                #if min_timestramp_s == 0:
-                #    min_timestramp_s = op_timestramp_ab_s
-                #else:
-                #    if op_timestramp_ab_s < min_timestramp_s:
-                #        min_timestramp_s = op_timestramp_ab_s
-                #if max_timestramp_s == 0:
-                #    max_timestramp_s = op_timestramp_ab_s
-                #else:
-                #    if op_timestramp_ab_s > max_timestramp_s:
-                #        max_timestramp_s = op_timestramp_ab_s
-                if op_type == "diskread":
-                    #total_read += 1
-                    #size_read += op_size
-                    #if op_timestramp_ab_s in count_s_2_read:
-                    #    count_s_2_read[op_timestramp_ab_s] += 1
+                    size_write += op_size
+                    #if op_timestramp_ab_s in count_s_2_write:
+                    #    count_s_2_write[op_timestramp_ab_s] += 1
                     #else:
-                    #    count_s_2_read.setdefault(op_timestramp_ab_s, 1)
-                    if op_size in count_byte_2_io:
-                        count_byte_2_io[op_size] += 1
-                    else:
-                        count_byte_2_io.setdefault(op_size, 1)
-                else:
-                    if op_type == "diskwrite":
-                        #total_write += 1
-                        #size_write += op_size
-                        #if op_timestramp_ab_s in count_s_2_write:
-                        #    count_s_2_write[op_timestramp_ab_s] += 1
-                        #else:
-                        #    count_s_2_write.setdefault(op_timestramp_ab_s, 1)
-                        if op_size in count_byte_2_io:
-                            count_byte_2_io[op_size] += 1
-                        else:
-                            count_byte_2_io.setdefault(op_size, 1)
-            if line == "endheader":
-                processing = True
-        #print("TotalRead  = " + str(total_read))
-        #print("TotalWrite = " + str(total_write))
-    #print("TotalRead  = " + str(total_read))
-    #print("TotalWrite = " + str(total_write))
-    #print("SizeRead = " + str(size_read) )
-    #print("SizeWrite = " + str(size_write) )
-    #print("MinTime = " + str(min_timestramp_s) )
-    #print("MaxTime = " + str(max_timestramp_s) )
-    #durning_time = max_timestramp_s - min_timestramp_s
-    #print("AvgIOPS = " + str( int((total_write + total_read)/(durning_time)) ) )
-    #print("AvgReadSize = " + str( int((size_read)/(durning_time)) ) )
-    #print("AvgWriteSize = " + str( int((size_write)/(durning_time)) ) )
-    #print("AvgReqSize = " + str( int((size_write + size_read)/(durning_time)) ) )
+                    #    count_s_2_write.setdefault(op_timestramp_ab_s, 1)
+        if line == "endheader":
+            processing = True
+    file.close()
+    print("TotalRead  = " + str(total_read))
+    print("TotalWrite = " + str(total_write))
+print("TotalRead  = " + str(total_read))
+print("TotalWrite = " + str(total_write))
+print("SizeRead = " + str(size_read) )
+print("SizeWrite = " + str(size_write) )
+print("MinTime = " + str(min_timestramp_s) )
+print("MaxTime = " + str(max_timestramp_s) )
+durning_time = max_timestramp_s - min_timestramp_s
+print("AvgIOPS = " + str( int((total_write + total_read)/(durning_time)) ) )
+print("AvgReadSize = " + str( int((size_read)/(durning_time)) ) )
+print("AvgWriteSize = " + str( int((size_write)/(durning_time)) ) )
+print("AvgReqSize = " + str( int((size_write + size_read)/(durning_time)) ) )
 
-    #count_s_2_io = count_s_2_read.copy()
-    #for w in count_s_2_write:
-    #    if w in count_s_2_io:
-    #        count_s_2_io[w] += count_s_2_write[w]
-    #    else:
-    #        count_s_2_io.setdefault(w, count_s_2_write[w])
+# count timestramp in second to io
+# which means to merged s_2_read & s_2_write
+#count_s_2_io = count_s_2_read.copy()
+#for w in count_s_2_write:
+#    if w in count_s_2_io:
+#        count_s_2_io[w] += count_s_2_write[w]
+#    else:
+#        count_s_2_io.setdefault(w, count_s_2_write[w])
 
-    result_s_2_read = default_result_dir + r"\\" + "s_2_read.csv"
-    result_s_2_write = default_result_dir + r"\\" + "s_2_write.csv"
-    result_s_2_io = default_result_dir + r"\\" + "s_2_io.csv"
-    result_byte_2_io = default_result_dir + r"\\" + "byte_2_io.csv"
+# calculate interarrival time of whole requests
+# and its data offset of sector
+'''
+keys = map_ns_to_offset_in_sector.keys()
+first = True
+ns_L = 0
+ns_H = 0
+count_inter_ns_2_io = dict()
+count_inter_offset_sector_2_io = dict()
+for ns in sorted(keys): # sorted timestramp in ns of request
+    if first:
+        ns_L = ns
+        first = False
+    else:
+        ns_H = ns
+        interarrival_time_ns = ns_H - ns_L
+        interarrival_offset_sector = map_ns_to_offset_in_sector[ns_H] - map_ns_to_offset_in_sector[ns_L]
+        if interarrival_time_ns in count_inter_ns_2_io:
+            count_inter_ns_2_io[ interarrival_time_ns ] += 1
+        else:
+            count_inter_ns_2_io.setdefault( interarrival_time_ns, 1 )
+        if interarrival_offset_sector in count_inter_offset_sector_2_io:
+            count_inter_offset_sector_2_io[ interarrival_offset_sector ] += 1
+        else:
+            count_inter_offset_sector_2_io.setdefault( interarrival_offset_sector, 1 )
+        ns_L = ns_H
+'''
+# construct output path of result
+result_s_2_read = default_result_dir + r"\\" + "s_2_read.csv"
+result_s_2_write = default_result_dir + r"\\" + "s_2_write.csv"
+result_s_2_io = default_result_dir + r"\\" + "s_2_io.csv"
+result_byte_2_io = default_result_dir + r"\\" + "byte_2_io.csv"
+result_inter_offset_sector_2_io = default_result_dir + r"\\" + "inter_offset_sector_2_io.csv"
+result_inter_ns_2_io = default_result_dir + r"\\" + "inter_ns_2_io.csv"
 
-    #write_dict_to_csv(result_s_2_read, count_s_2_read)
-    #write_dict_to_csv(result_s_2_write, count_s_2_write)
-    #write_dict_to_csv(result_s_2_io, count_s_2_io)
-    write_dict_to_csv(result_byte_2_io, count_byte_2_io)
+# write data to file
+#write_dict_to_csv(result_s_2_read, count_s_2_read)
+#write_dict_to_csv(result_s_2_write, count_s_2_write)
+#write_dict_to_csv(result_s_2_io, count_s_2_io)
+#write_dict_to_csv(result_byte_2_io, count_byte_2_io)
+#write_dict_to_csv(result_inter_offset_sector_2_io, count_inter_offset_sector_2_io)
+#write_dict_to_csv(result_inter_ns_2_io, count_inter_ns_2_io)
 
-#end of script
-log_close()
+for disknum in map_disknum_2_s_2_io:
+    result_s_2_io_unber_disknum = default_result_dir + r"\\" + "disk_" + str(disknum) + "_s_2_io.csv"
+    s_2_io = map_disknum_2_s_2_io[disknum]
+    write_dict_to_csv(result_s_2_io_unber_disknum, s_2_io)
--- iotrace_draw.py
+++ iotrace_draw.py
@@ -1,3 +1,4 @@
+from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import matplotlib.mlab as mlab
 import matplotlib.pyplot as plt
@@ -10,6 +11,8 @@ result_s_2_read = default_result_dir + r"\\" + "s_2_read.csv"
 result_s_2_write = default_result_dir + r"\\" + "s_2_write.csv"
 result_s_2_io = default_result_dir + r"\\" + "s_2_io.csv"
 result_byte_2_io = default_result_dir + r"\\" + "byte_2_io.csv"
+
+#spread of io size
 '''
 b_2_io = open(result_byte_2_io,"r")
 b = []
@@ -44,7 +47,7 @@ count = 0
 while count < b.__len__():
     byte = b[count]
     num = io[count]
-    index = int(byte/1024/4)
+    index = int(byte/1024/4)#4KB each
     if index > 17:
         index = 17
     io_spread[index] += num
@@ -52,7 +55,7 @@ while count < b.__len__():
 print(io_spread)
 '''
 
-
+#number of reads and writes per second
 '''
 s_2_r = open(result_s_2_read, "r")
 s_2_w = open(result_s_2_write, "r")
@@ -98,9 +101,8 @@ plt.legend(loc='upper right')
 plt.show()
 '''
 
-
-
-
+#number of requests
+'''
 s_2_io = open(result_s_2_io, "r")
 s = []
 io = []
@@ -114,6 +116,8 @@ for line in s_2_io:
     io.append(c)
     total_io += c
 s_2_io.close()
+'''
+#per second
 """
 fig_s_2_io = plt.figure()
 ax = fig_s_2_io.add_subplot(1,1,1)
@@ -125,6 +129,8 @@ plt.ylim(0,3000)
 ax.bar(s, io, alpha = .5, color = 'k')
 plt.show()
 """
+#per hour
+'''
 h_2_io = dict()
 index = 0
 while index < s.__len__():
@@ -146,4 +152,31 @@ ax.set_ylabel('Total Requests (I/Os)')
 ax.set_xlabel('DevelopmentToolsRelease')
 ax.set_title('Number of requests per hour')
 ax.bar(h, io, alpha = .5, color = 'k')
+plt.show()
+'''
+
+# 3D disknum to time in second to io
+# 17 disk for all from 0 to 16
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+ax.set_xlabel("time per second")
+ax.set_ylabel('number of requests')
+ax.set_zlabel('disk number')
+for disknum ,cs in zip(
+        [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,16],
+        ['r', 'g', 'b', 'y','r', 'g', 'b', 'y','r', 'g', 'b', 'y','r', 'g', 'b', 'y']):
+    result_s_2_io_under_disknum = default_result_dir + r"\\" + "disk_" + str(disknum) + "_s_2_io.csv"
+    print( result_s_2_io_under_disknum )
+    f = open(result_s_2_io_under_disknum, "r")
+    xs = []
+    ys = []
+    for line in f:
+        words = line.strip().split(",")
+        time_s = int( words.__getitem__(0) ) - 1204736457 + 1
+        io = int( words.__getitem__(1) )
+        xs.append( time_s )
+        ys.append( io )
+    f.close()
+    print("Drawing it ......")
+    ax.bar(xs, ys, zs=disknum, zdir='y', color=cs, alpha=0.8)
 plt.show()
